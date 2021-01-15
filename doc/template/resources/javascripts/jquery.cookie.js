dee0d647bc24df997074191511056ae803e32053
Inspecode <inspecode-bot@rocro.com>
2021-01-15 05:11:33 +0000

    Fix misspell errors

--- doc/template/resources/javascripts/jquery.cookie.js
+++ doc/template/resources/javascripts/jquery.cookie.js
@@ -31,7 +31,7 @@
  * a date in the past), the cookie will be deleted. If set to null or omitted,
  * the cookie will be a session cookie and will not be retained when the the
  * browser exits.
- * @option String path The value of the path atribute of the cookie (default:
+ * @option String path The value of the path attribute of the cookie (default:
  * path of page that created the cookie).
  * @option String domain The value of the domain attribute of the cookie
  * (default: domain of page that created the cookie).
