b1e5081df332ebb01b8ece97f33ccb228b41be70
Inspecode <inspecode-bot@rocro.com>
2021-02-08 03:27:38 +0000

    Fix ClangFormat errors

--- api/resources/javascripts/bootstrap.js
+++ api/resources/javascripts/bootstrap.js
@@ -17,136 +17,128 @@
  * limitations under the License.
  * ============================================================ */
 
-
-!function ($) {
-
+!function($) {
   "use strict"; // jshint ;_;
 
+  /* DROPDOWN CLASS DEFINITION
+   * ========================= */
 
- /* DROPDOWN CLASS DEFINITION
-  * ========================= */
-
-  var toggle = '[data-toggle=dropdown]'
-    , Dropdown = function (element) {
+  var toggle = '[data-toggle=dropdown]',
+      Dropdown =
+          function(element) {
         var $el = $(element).on('click.dropdown.data-api', this.toggle)
-        $('html').on('click.dropdown.data-api', function () {
-          $el.parent().removeClass('open')
-        })
+        $('html').on('click.dropdown.data-api',
+                     function() { $el.parent().removeClass('open') })
       }
 
-  Dropdown.prototype = {
+          Dropdown.prototype =
+              {
 
-    constructor: Dropdown
+                constructor : Dropdown
 
-  , toggle: function (e) {
-      var $this = $(this)
-        , $parent
-        , isActive
+                ,
+                toggle : function(e) {
+                  var $this = $(this), $parent, isActive
 
-      if ($this.is('.disabled, :disabled')) return
+                  if ($this.is('.disabled, :disabled')) return
 
-      $parent = getParent($this)
+                      $parent = getParent($this)
 
-      isActive = $parent.hasClass('open')
+                  isActive = $parent.hasClass('open')
 
-      clearMenus()
+                  clearMenus()
 
-      if (!isActive) {
-        $parent.toggleClass('open')
-        $this.focus()
-      }
+                  if (!isActive) {
+                    $parent.toggleClass('open')
+                    $this.focus()
+                  }
 
-      return false
-    }
+                  return false
+                }
 
-  , keydown: function (e) {
-      var $this
-        , $items
-        , $active
-        , $parent
-        , isActive
-        , index
+                ,
+                keydown : function(e) {
+                  var $this, $items, $active, $parent, isActive, index
 
-      if (!/(38|40|27)/.test(e.keyCode)) return
+                  if (!/(38|40|27)/.test(e.keyCode)) return
 
-      $this = $(this)
+                      $this = $(this)
 
-      e.preventDefault()
-      e.stopPropagation()
+                  e.preventDefault()
+                  e.stopPropagation()
 
-      if ($this.is('.disabled, :disabled')) return
+                  if ($this.is('.disabled, :disabled')) return
 
-      $parent = getParent($this)
+                      $parent = getParent($this)
 
-      isActive = $parent.hasClass('open')
+                  isActive = $parent.hasClass('open')
 
-      if (!isActive || (isActive && e.keyCode == 27)) return $this.click()
+                  if (!isActive ||
+                      (isActive && e.keyCode == 27)) return $this.click()
 
-      $items = $('[role=menu] li:not(.divider) a', $parent)
+                  $items = $('[role=menu] li:not(.divider) a', $parent)
 
-      if (!$items.length) return
+                  if (!$items.length) return
 
-      index = $items.index($items.filter(':focus'))
+                      index = $items.index($items.filter(':focus'))
 
-      if (e.keyCode == 38 && index > 0) index--                                        // up
-      if (e.keyCode == 40 && index < $items.length - 1) index++                        // down
-      if (!~index) index = 0
+                  if (e.keyCode == 38 && index > 0) index-- // up
+                      if (e.keyCode == 40 && index < $items.length - 1)
+                          index++ // down
+                      if (!~index) index = 0
 
-      $items
-        .eq(index)
-        .focus()
-    }
+                  $items.eq(index).focus()
+                }
 
-  }
+              }
 
-  function clearMenus() {
-    getParent($(toggle))
-      .removeClass('open')
-  }
+              function
+              clearMenus() { getParent($(toggle)).removeClass('open') }
 
-  function getParent($this) {
-    var selector = $this.attr('data-target')
-      , $parent
+              function getParent($this) {
+                var selector = $this.attr('data-target'), $parent
 
-    if (!selector) {
-      selector = $this.attr('href')
-      selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
-    }
+                if (!selector) {
+                  selector = $this.attr('href')
+                  selector =
+                      selector && /#/.test(selector) &&
+                      selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+                }
 
-    $parent = $(selector)
-    $parent.length || ($parent = $this.parent())
+                $parent = $(selector)
+                $parent.length || ($parent = $this.parent())
 
-    return $parent
-  }
+                return $parent
+              }
 
+              /* DROPDOWN PLUGIN DEFINITION
+               * ========================== */
 
-  /* DROPDOWN PLUGIN DEFINITION
-   * ========================== */
+              $.fn.dropdown =
+                  function(option) {
+                return this.each(function() {
+                  var $this = $(this), data = $this.data('dropdown')
+                  if (!data) $this.data('dropdown', (data = new Dropdown(this)))
+                  if (typeof option == 'string') data[option].call($this)
+                })
+              }
 
-  $.fn.dropdown = function (option) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('dropdown')
-      if (!data) $this.data('dropdown', (data = new Dropdown(this)))
-      if (typeof option == 'string') data[option].call($this)
-    })
-  }
-
-  $.fn.dropdown.Constructor = Dropdown
-
+                  $.fn.dropdown.Constructor = Dropdown
 
   /* APPLY TO STANDARD DROPDOWN ELEMENTS
    * =================================== */
 
-  $(function () {
-    $('html')
-      .on('click.dropdown.data-api touchstart.dropdown.data-api', clearMenus)
+  $(function() {
+    $('html').on('click.dropdown.data-api touchstart.dropdown.data-api',
+                 clearMenus)
     $('body')
-      .on('click.dropdown touchstart.dropdown.data-api', '.dropdown form', function (e) { e.stopPropagation() })
-      .on('click.dropdown.data-api touchstart.dropdown.data-api'  , toggle, Dropdown.prototype.toggle)
-      .on('keydown.dropdown.data-api touchstart.dropdown.data-api', toggle + ', [role=menu]' , Dropdown.prototype.keydown)
+        .on('click.dropdown touchstart.dropdown.data-api', '.dropdown form',
+            function(e) { e.stopPropagation() })
+        .on('click.dropdown.data-api touchstart.dropdown.data-api', toggle,
+            Dropdown.prototype.toggle)
+        .on('keydown.dropdown.data-api touchstart.dropdown.data-api',
+            toggle + ', [role=menu]', Dropdown.prototype.keydown)
   })
-
 }(window.jQuery);
 /* ========================================================
  * bootstrap-tab.js v2.1.1
@@ -167,121 +159,107 @@
  * limitations under the License.
  * ======================================================== */
 
-
-!function ($) {
-
+!function($) {
   "use strict"; // jshint ;_;
 
+  /* TAB CLASS DEFINITION
+   * ==================== */
 
- /* TAB CLASS DEFINITION
-  * ==================== */
+  var Tab = function(element) { this.element = $(element) }
 
-  var Tab = function (element) {
-    this.element = $(element)
-  }
+            Tab.prototype = {
 
-  Tab.prototype = {
+    constructor : Tab
 
-    constructor: Tab
-
-  , show: function () {
-      var $this = this.element
-        , $ul = $this.closest('ul:not(.dropdown-menu)')
-        , selector = $this.attr('data-target')
-        , previous
-        , $target
-        , e
+    ,
+    show : function() {
+      var $this = this.element, $ul = $this.closest('ul:not(.dropdown-menu)'),
+          selector = $this.attr('data-target'), previous, $target, e
 
       if (!selector) {
         selector = $this.attr('href')
-        selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
+        selector =
+            selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
       }
 
-      if ( $this.parent('li').hasClass('active') ) return
+      if ($this.parent('li').hasClass('active'))
+        return
 
-      previous = $ul.find('.active a').last()[0]
+            previous = $ul.find('.active a').last()[0]
 
-      e = $.Event('show', {
-        relatedTarget: previous
-      })
+        e = $.Event('show', {relatedTarget : previous})
 
-      $this.trigger(e)
+        $this.trigger(e)
 
-      if (e.isDefaultPrevented()) return
+        if (e.isDefaultPrevented()) return
 
-      $target = $(selector)
+            $target = $(selector)
 
-      this.activate($this.parent('li'), $ul)
-      this.activate($target, $target.parent(), function () {
-        $this.trigger({
-          type: 'shown'
-        , relatedTarget: previous
-        })
-      })
+                          this.activate($this.parent('li'), $ul) this.activate(
+                              $target, $target.parent(), function() {
+                                $this.trigger(
+                                    {type : 'shown', relatedTarget : previous})
+                              })
     }
 
-  , activate: function ( element, container, callback) {
-      var $active = container.find('> .active')
-        , transition = callback
-            && $.support.transition
-            && $active.hasClass('fade')
+    ,
+    activate : function(element, container, callback) {
+      var $active = container.find('> .active'),
+          transition =
+              callback && $.support.transition && $active.hasClass('fade')
 
       function next() {
-        $active
-          .removeClass('active')
-          .find('> .dropdown-menu > .active')
-          .removeClass('active')
+        $active.removeClass('active')
+            .find('> .dropdown-menu > .active')
+            .removeClass('active')
 
         element.addClass('active')
 
         if (transition) {
           element[0].offsetWidth // reflow for transition
           element.addClass('in')
-        } else {
+        }
+        else {
           element.removeClass('fade')
         }
 
-        if ( element.parent('.dropdown-menu') ) {
+        if (element.parent('.dropdown-menu')) {
           element.closest('li.dropdown').addClass('active')
         }
 
         callback && callback()
       }
 
-      transition ?
-        $active.one($.support.transition.end, next) :
-        next()
+      transition ? $active.one($.support.transition.end, next) : next()
 
       $active.removeClass('in')
     }
   }
 
+                            /* TAB PLUGIN DEFINITION
+                             * ===================== */
 
- /* TAB PLUGIN DEFINITION
-  * ===================== */
-
-  $.fn.tab = function ( option ) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('tab')
+                            $.fn.tab =
+      function(option) {
+    return this.each(function() {
+      var $this = $(this), data = $this.data('tab')
       if (!data) $this.data('tab', (data = new Tab(this)))
       if (typeof option == 'string') data[option]()
     })
   }
 
-  $.fn.tab.Constructor = Tab
+      $.fn.tab.Constructor = Tab
 
+  /* TAB DATA-API
+   * ============ */
 
- /* TAB DATA-API
-  * ============ */
-
-  $(function () {
-    $('body').on('click.tab.data-api', '[data-toggle="tab"], [data-toggle="pill"]', function (e) {
-      e.preventDefault()
-      $(this).tab('show')
-    })
+  $(function() {
+    $('body').on('click.tab.data-api',
+                 '[data-toggle="tab"], [data-toggle="pill"]', function(e) {
+                   e.preventDefault()
+                   $(this).tab('show')
+                 })
   })
-
 }(window.jQuery);
 /* ==========================================================
  * bootstrap-affix.js v2.1.1
@@ -302,88 +280,89 @@
  * limitations under the License.
  * ========================================================== */
 
-
-!function ($) {
-
+!function($) {
   "use strict"; // jshint ;_;
 
+  /* AFFIX CLASS DEFINITION
+   * ====================== */
 
- /* AFFIX CLASS DEFINITION
-  * ====================== */
-
-  var Affix = function (element, options) {
-    this.options = $.extend({}, $.fn.affix.defaults, options)
-    this.$window = $(window).on('scroll.affix.data-api', $.proxy(this.checkPosition, this))
-    this.$element = $(element)
-    this.checkPosition()
+  var Affix =
+      function(element, options) {
+    this.options = $.extend({}, $.fn.affix.defaults, options) this.$window =
+        $(window)
+            .on('scroll.affix.data-api',
+                $.proxy(this.checkPosition, this)) this.$element =
+            $(element) this.checkPosition()
   }
 
-  Affix.prototype.checkPosition = function () {
-    if (!this.$element.is(':visible')) return
+      Affix.prototype.checkPosition =
+          function() {
+    if (!this.$element.is(':visible'))
+      return
 
-    var scrollHeight = $(document).height()
-      , scrollTop = this.$window.scrollTop()
-      , position = this.$element.offset()
-      , offset = this.options.offset
-      , offsetBottom = offset.bottom
-      , offsetTop = offset.top
-      , reset = 'affix affix-top affix-bottom'
-      , affix
+          var scrollHeight = $(document).height(),
+              scrollTop = this.$window.scrollTop(),
+              position = this.$element.offset(), offset = this.options.offset,
+              offsetBottom = offset.bottom, offsetTop = offset.top,
+              reset = 'affix affix-top affix-bottom', affix
 
-    if (typeof offset != 'object') offsetBottom = offsetTop = offset
-    if (typeof offsetTop == 'function') offsetTop = offset.top()
-    if (typeof offsetBottom == 'function') offsetBottom = offset.bottom()
+      if (typeof offset != 'object') offsetBottom = offsetTop = offset
+      if (typeof offsetTop == 'function') offsetTop = offset.top()
+      if (typeof offsetBottom == 'function') offsetBottom = offset.bottom()
 
-    affix = this.unpin != null && (scrollTop + this.unpin <= position.top) ?
-      false    : offsetBottom != null && (position.top + this.$element.height() >= scrollHeight - offsetBottom) ?
-      'bottom' : offsetTop != null && scrollTop <= offsetTop ?
-      'top'    : false
+      affix = this.unpin != null && (scrollTop + this.unpin <= position.top)
+                  ? false
+                  : offsetBottom != null &&
+                            (position.top + this.$element.height() >=
+                             scrollHeight - offsetBottom)
+                        ? 'bottom'
+                        : offsetTop != null && scrollTop <= offsetTop ? 'top'
+                                                                      : false
 
-    if (this.affixed === affix) return
+      if (this.affixed === affix) return
 
-    this.affixed = affix
-    this.unpin = affix == 'bottom' ? position.top - scrollTop : null
+          this.affixed = affix this.unpin =
+          affix == 'bottom'
+              ? position.top - scrollTop
+              : null
 
-    this.$element.removeClass(reset).addClass('affix' + (affix ? '-' + affix : ''))
+                this.$element.removeClass(reset)
+                    .addClass('affix' + (affix ? '-' + affix : ''))
   }
 
+          /* AFFIX PLUGIN DEFINITION
+           * ======================= */
 
- /* AFFIX PLUGIN DEFINITION
-  * ======================= */
-
-  $.fn.affix = function (option) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('affix')
-        , options = typeof option == 'object' && option
+          $.fn.affix =
+              function(option) {
+    return this.each(function() {
+      var $this = $(this), data = $this.data('affix'),
+          options = typeof option == 'object' && option
       if (!data) $this.data('affix', (data = new Affix(this, options)))
       if (typeof option == 'string') data[option]()
     })
   }
 
-  $.fn.affix.Constructor = Affix
+              $.fn.affix.Constructor = Affix
 
-  $.fn.affix.defaults = {
-    offset: 0
-  }
+  $.fn.affix.defaults = {offset : 0}
 
+                        /* AFFIX DATA-API
+                         * ============== */
 
- /* AFFIX DATA-API
-  * ============== */
+                        $(window)
+                            .on('load', function() {
+                              $('[data-spy="affix"]').each(function() {
+                                var $spy = $(this), data = $spy.data()
 
-  $(window).on('load', function () {
-    $('[data-spy="affix"]').each(function () {
-      var $spy = $(this)
-        , data = $spy.data()
+                                data.offset = data.offset || {}
 
-      data.offset = data.offset || {}
+                                data.offsetBottom &&
+                                    (data.offset.bottom = data.offsetBottom)
+                                data.offsetTop &&
+                                    (data.offset.top = data.offsetTop)
 
-      data.offsetBottom && (data.offset.bottom = data.offsetBottom)
-      data.offsetTop && (data.offset.top = data.offsetTop)
-
-      $spy.affix(data)
-    })
-  })
-
-
+                                $spy.affix(data)
+                              })
+                            })
 }(window.jQuery);
